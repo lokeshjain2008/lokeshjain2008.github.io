3a02332e0f3741c23547dad6be2fb43203a75aa4
lokesh kumar jain <ljain@lokeshs-MacBook-Air.local>
2015-02-17 00:22:59 +0530

    added excerpt to the post list

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -1,16 +1,3 @@
-# Sitemap.xml Generator is a Jekyll plugin that generates a sitemap.xml file by 
-# traversing all of the available posts and pages.
-# 
-# See readme file for documenation
-# 
-# Updated to use config file for settings by Daniel Groves
-# Site: http://danielgroves.net
-# 
-# Author: Michael Levin
-# Site: http://www.kinnetica.com
-# Distributed Under A Creative Commons License
-#   - http://creativecommons.org/licenses/by/3.0/
-
 require 'rexml/document'
 
 module Jekyll
